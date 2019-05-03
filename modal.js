bc39c29dbf1e6ecdb3afe31fa5f2dd36188d4f28
comjun04 <30339539+comjun04@users.noreply.github.com>
2019-05-03 23:06:08 +0900

    Pack Information Save/Load feature Finally completed it

---
+++ modal.js
@@ -0,0 +1,21 @@
+// sounds Manager Modal Box
+var soundsManagerModal = document.getElementById('sounds-manager');
+
+function soundsManagerOpen() {
+	soundsManagerModal.style.display = "block";
+}
+
+function soundsManagerClose() {
+	soundsManagerModal.style.display = "none";
+}
+
+// Information Edit Modal Box
+var packInfoModal = document.getElementById('packInfoModal');
+
+function packInfoOpen() {
+	packInfoModal.style.display = "block";
+}
+
+function packInfoClose() {
+	packInfoModal.style.display = "none";
+}
--- script.js
+++ script.js
@@ -1,42 +1,19 @@
-// JavaScript Document
-
 // Sidebar
 function sidebarOpen() {
-	let obj = document.getElementById('sidebar');
+	var obj = document.getElementById('sidebar');
 	obj.style.width = '100%';
 	// obj.style.display = 'block';
 }
+
 function sidebarClose() {
 	document.getElementById('sidebar').style.width = 0;
-}
-
-// sounds Manager Modal Box
-var soundsManagerModal = document.getElementById('sounds-manager');
-
-function soundsManagerOpen() {
-	soundsManagerModal.style.display = "block";
-}
-
-function soundsManagerClose() {
-	soundsManagerModal.style.display = "none";
-}
-
-// Information Edit Modal Box
-var packInfoModal = document.getElementById('packInfoModal');
-
-function packInfoOpen() {
-	packInfoModal.style.display = "block";
-}
-
-function packInfoClose() {
-	packInfoModal.style.display = "none";
 }
 
 // soundList
 var soundListAddBtn = document.getElementById('soundListAddBtn');
 var soundListInputFile = document.getElementById('soundListInputFile');
-soundListAddBtn.onclick = function() {
-	
+soundListAddBtn.onclick = function () {
+
 }
 
 var buttonPressedX = 0;
@@ -45,13 +22,12 @@ var buttonPressedMC = 0;
 
 function buttonSoundsModalOpen(x_mc, y) {
 	var buttonPos = "";
-	if(y == 0) // mc button
+	if (y == 0) // mc button
 	{
 		buttonPressedMC = x_mc;
 		buttonPressedY = 0;
 		buttonPos = "mc" + x_mc;
-	}
-	else {
+	} else {
 		buttonPressedX = x_mc;
 		buttonPressedY = y;
 		buttonPos = "(" + y + ", " + x_mc + ")";
@@ -62,4 +38,4 @@ function buttonSoundsModalOpen(x_mc, y) {
 
 function buttonSoundsModalClose() {
 	document.getElementById('buttonSoundsModal').style.display = 'none';
-	}
+}
